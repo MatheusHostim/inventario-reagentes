4c4dc7152f4b95d0a282452b3b2fe49f4ec37f0d
MatheusHostim <71354775+MatheusHostim@users.noreply.github.com>
2025-11-09 23:18:48 +0000

    3

--- inv-reagentes/src/main/java/com/sistemaempresa/invreagentes/InvReagentesApplication.java
+++ inv-reagentes/src/main/java/com/sistemaempresa/invreagentes/InvReagentesApplication.java
@@ -1,4 +1,4 @@
-package com.sistemaempresa.inv_reagentes;
+package com.sistemaempresa.invreagentes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvReagentesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvReagentesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InvReagentesApplication.class, args);
+    }
 
 }
